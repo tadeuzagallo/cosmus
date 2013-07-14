b6304f56f96536b83fd83ca0156190f9bf3b51f8
tadeuzagallo <tadeuzagallo@gmail.com>
2013-07-14 18:59:38 -0300

    fix flickering with integer radius

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
 ;(function(w, d, undefined) { 
   var canvas = d.getElementById('c'),
       ctx = canvas.getContext('2d');
+
   canvas.width = canvas.width || canvas.clientWidht;
   canvas.height = canvas.height || canvas.clientHeight;
-  
+
   var backCanvas = d.createElement('canvas'),
       backCtx = backCanvas.getContext('2d');
 
@@ -24,23 +25,51 @@
         this.texture = false;
       }
 
-      this.radius = this.texture ? Math.round(radius) : radius;
+      this.radius = Math.round(radius);
       this.diameter = this.radius * 2;
       this.shadow = shadow ? true : false;
     }
 
+    a = 0;
     Astro.prototype.draw = function(offset) {
       if (Astro.texture && this.texture) {
-        backCtx.clearRect(0, 0, this.diameter, this.diameter);
+        var x, y, _x, _y, width, height;
+        x = Math.floor(this.x);
+        y = Math.floor(this.y);
+        _x = this.x - x;
+        _y = this.y - y;
+        width = Math.ceil(this.radius + _x) * 2;
+        height = Math.ceil(this.radius + _y) * 2;
+
+        backCtx.clearRect(0, 0, width, height);
         backCtx.save();
 
-        backCtx.translate(this.radius, this.radius);
-        backCtx.rotate(this.rotation);
-        backCtx.translate(-this.radius, -this.radius);
+        backCtx.translate(
+          this.radius + _x,
+          this.radius + _y
+        );
+
+        backCtx.rotate(
+          this.rotation
+        );
+
+        backCtx.translate(
+          -(this.radius + _x),
+          -(this.radius + _y)
+        );
 
         backCtx.beginPath();
+
+        backCtx.arc(
+          (this.radius + _x),
+          (this.radius + _y),
+          this.radius,
+          0,
+          Math.PI * 2,
+          false
+        );
+
         backCtx.fillStyle = this.color;
-        backCtx.arc(this.radius, this.radius, this.radius, 0, Math.PI * 2, false);
         backCtx.fill();
 
         backCtx.fillStyle = Astro.texture;
@@ -48,10 +77,14 @@
 
         backCtx.closePath();
 
-        var image = backCtx.getImageData(0, 0, this.diameter, this.diameter);
+        var image = backCtx.getImageData(0, 0, width, height);
         backCtx.restore();
 
-        ctx.putImageData(image, this.x - this.radius, this.y - this.radius);
+        ctx.putImageData(
+          image,
+          x - this.radius,
+          y - this.radius
+        );
 
         if (this.shadow) {
           ctx.beginPath();
@@ -66,7 +99,13 @@
           grd.addColorStop(0, 'rgba(0,0,0,.3)');
           grd.addColorStop(1, 'rgba(0,0,0,1)');
 
-          ctx.arc(this.x, this.y, this.radius * 1.03, 0, Math.PI * 2, false);
+          ctx.arc(
+            this.x, 
+            this.y,
+            this.radius * 1.04,
+            0,
+            Math.PI * 2,
+            false);
 
           ctx.fillStyle = grd;
           ctx.fill();
@@ -92,13 +131,15 @@
 
   var Galaxy = function() {
     var stars = [],
-        astros = [];
+    astros = [];
     return {
       add_star: function(star) {
         stars.push(star);
+        return this;
       },
       add: function(astro) {
         astros.push(astro);
+        return this;
       },
       update: function() {
         for (var k = 0, l = astros.length; k < l; k++) {
@@ -110,10 +151,11 @@
             astro.rotation += Math.PI / 10 * astro.velocity;
             astro.rotation %= Math.PI * 2;
 
-            astro.x = Math.round(astro.parent.x + Math.cos(astro.step) * (astro.translate_radius + astro.parent.radius + astro.radius));
-            astro.y = Math.round(astro.parent.y + Math.sin(astro.step) * (astro.translate_radius + astro.parent.radius + astro.radius));
+            astro.x = astro.parent.x + Math.cos(astro.step) * (astro.translate_radius + astro.parent.radius + astro.radius);
+            astro.y = astro.parent.y + Math.sin(astro.step) * (astro.translate_radius + astro.parent.radius + astro.radius);
           }
         }
+        return this;
       },
       draw: function() {
         for (var k = 0, l = stars.length; k < l; k++) {
@@ -123,43 +165,58 @@
         for (var k = 0, l = astros.length; k < l; k++) {
           astros[k].draw();
         }
+        return this;
       },
       star_offset: {x: 0, y: 0}
     }
   }();
 
   canvas.onmousemove = function(e) {
-    var o = Galaxy.star_offset;
-    Galaxy.star_offset = {x: o.x + ((e.pageX * .025 - o.x) % 2), y: o.y + ((e.pageY * 0.025 - o.y) % 2)};
+    Galaxy.star_offset = {
+      x: (e.pageX - this.width / 2) * 0.025,
+      y: (e.pageY - this.height / 2) * 0.025
+    };
   };
 
-  var sun = new Astro(20, 'yellow'),
-      mercury = new Astro(2.4, '#3F332C', sun, 50, .2, true),
-      venus = new Astro(6.05, '#CEB735', sun, 61, .3, true),
+  var sun = new Astro(20, '#C0C000'),
+      mercury = new Astro(3, '#3F332C', sun, 50, .2, true),
+      venus = new Astro(6, '#CEB735', sun, 61, .3, true),
 
-      earth = new Astro(6.37, 'green', sun, 95, .1, true),
-        moon = new Astro(2, 'white', earth, 5.2, .4),
+      earth = new Astro(7, '#00DD00', sun, 95, .1, true),
+        moon = new Astro(3, '#DDDDDD', earth, 5.2, .4),
 
-      mars = new Astro(3.4, 'red', sun, 133, .15, true),
-      jupiter = new Astro(10, '#984520', sun, 170, .05, true),
+      mars = new Astro(4, 'red', sun, 133, .15, true),
+      jupiter = new Astro(10, '#984520', sun, 170, .1, true),
       saturn = new Astro(8, '#C5B156', sun, 216, .18, true),
-      uranus = new Astro(7.5, '#4C88A4', sun, 248, .13, true),
-      neptune = new Astro(7, '#192850', sun, 290, .05, true);
+      uranus = new Astro(7, '#4C88A4', sun, 248, .13, true),
+      neptune = new Astro(6, '#192850', sun, 290, .05, true);
 
   sun.x = canvas.width / 2;
   sun.y = canvas.height / 2;
 
   for (var k = 0; k < 10; k++) {
-    var explosion_core = new Astro(1, 'transparent', sun, 0, .5 + Math.random());
-    var explosion = new Astro(.5 + Math.random() * .5,
-                              '#eebb00',
-                              explosion_core,
-                              .2 + Math.random() * .2,
-                              3 + Math.random() * 3,
-                              false,
-                              false);
-    Galaxy.add(explosion_core);
-    Galaxy.add(explosion);
+    var explosion_core = new Astro(
+      1,
+      'transparent',
+      sun,
+      0,
+      .5 + Math.random(),
+      false,
+      false
+    ),
+    explosion = new Astro(
+      .5 + Math.random() * .5,
+      '#eebb00',
+      explosion_core,
+      .2 + Math.random() * .2,
+      3 + Math.random() * 3,
+      false,
+      false
+    );
+
+    Galaxy
+    .add(explosion_core)
+    .add(explosion);
   }
 
   for (var k = 0; k < 100; k++) {
@@ -169,21 +226,24 @@
     Galaxy.add_star(star);
   };
 
-  Galaxy.add(sun);
-  Galaxy.add(mercury);
-  Galaxy.add(venus);
-  Galaxy.add(earth);
-  Galaxy.add(mars);
-  Galaxy.add(jupiter);
-  Galaxy.add(saturn);
-  Galaxy.add(uranus);
-  Galaxy.add(neptune);
-  Galaxy.add(moon);
+  Galaxy
+  .add(sun)
+  .add(mercury)
+  .add(venus)
+  .add(earth)
+  .add(mars)
+  .add(jupiter)
+  .add(saturn)
+  .add(uranus)
+  .add(neptune)
+  .add(moon);
 
   (function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    Galaxy.update();
-    Galaxy.draw();
+    Galaxy
+    .update()
+    .draw();
+
     w.requestAnimationFrame(update);
   })();
 })(window, document);
